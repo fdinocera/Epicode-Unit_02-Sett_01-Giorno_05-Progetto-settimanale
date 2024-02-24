2e6df4c6f7675ac8d5384d92169af85ea6125aba
Ferdinando <fdinocera@libero.it>
2024-02-24 09:11:20 +0100

    ultimato desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,34 @@
 
 //globali html
-let testata = document.getElementById('testata');
-let getStarted = document.getElementById('getStarted');
+const testata = document.getElementById('testata');
+const getStarted = document.getElementById('getStarted');
+const ftFooter = document.getElementById('ftFooter');
+
 
 
 addEventListener('scroll', (e) => {
 
     if (window.scrollY >= 420) {
-
         //TESTATA BIANCA
-        testata.style.backgroundColor = "white";
+        testata.style.backgroundColor = 'rgb(255, 255, 255)';
 
-        //PULSANTE VERDE
-        // getStarted.style.backgroundColor = rgb(21, 109, 18);   
-        getStarted.style.backgroundColor = rgb(255, 0, 0);
+        //PULSANTE VERDE        
+        getStarted.style.backgroundColor = 'rgb(21, 109, 18)';
+
+        //animation: fadeInAnimation 2s;
+        getStarted.style.animation = 'fadeInAnimation';
+        getStarted.style.animationDuration = '3s';
 
 
     } else {
         //TESTATA GIALLA
-        testata.style.backgroundColor = rgb(255, 192, 23);
+        testata.style.backgroundColor = 'rgb(255, 192, 23)';
 
         //PULSANTE NERO
-        getStarted.style.backgroundColor = rgb(19, 19, 19);
+        getStarted.style.backgroundColor = 'rgb(19, 19, 19)';
+
+        getStarted.style.animation = 'fadeInAnimation';
+        getStarted.style.animationDuration = '3s';
+        getStarted.style.animationDirection = 'reverse';
     }
 });
