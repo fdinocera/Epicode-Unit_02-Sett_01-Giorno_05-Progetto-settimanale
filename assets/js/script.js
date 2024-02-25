d8459a8544f9d9a92b6e448917a7d5eb3e7f2284
Ferdinando <fdinocera@libero.it>
2024-02-25 08:39:15 +0100

    Aggiunta animazione delle EMME

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
-
 //globali html
 const testata = document.getElementById('testata');
 const getStarted = document.getElementById('getStarted');
-// const ftFooter = document.getElementById('ftFooter');
-
-
 
 addEventListener('scroll', (e) => {
 
@@ -31,3 +27,43 @@ addEventListener('scroll', (e) => {
         getStarted.style.animationDirection = 'reverse';
     }
 });
+
+function init() {
+    const nEmme = 31;
+    const righe = 15;
+    const divEmme = document.getElementById('emme');
+
+    for (let j = 0; j < righe; j++) {
+        let newDiv = document.createElement('div');
+        for (let i = 0; i < nEmme; i++) {
+
+            let newSpan = document.createElement('span');
+            newSpan.innerText = 'M';
+
+            const random = Math.floor(Math.random() * 2);
+            if (random === 0) {
+                newSpan.style.opacity = '0';
+            } else {
+                newSpan.style.opacity = '1';
+            }
+            newDiv.appendChild(newSpan);
+        }
+        divEmme.appendChild(newDiv);
+    }
+}
+
+function sostituzioneRandom() {
+
+    let emmeElements = document.querySelectorAll('#emme div span');
+
+    const random = Math.floor(Math.random() * emmeElements.length);
+
+    if (emmeElements[random].style.opacity === '1') {
+        emmeElements[random].style.opacity = '0';
+    } else {
+        emmeElements[random].style.opacity = '1';
+    }
+}
+
+init();
+setInterval(sostituzioneRandom, 40);
